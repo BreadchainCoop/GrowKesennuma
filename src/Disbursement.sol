ccedd8ce5d9aa1540b7973a289e6fbdd5a2dfc52
hudsonhrh <hudsonhrh7@gmail.com>
2025-05-12 16:29:04 -0400

    chore: remove unneeded import

--- src/Disbursement.sol
+++ src/Disbursement.sol
@@ -5,7 +5,6 @@ import "./Impactor.sol";
 import "./Governance.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract Disbursement {
     using SafeERC20 for IERC20;
